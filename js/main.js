9b24a7b10b685bc1c8d27f2d3d3a26982c26d374
Andrew <andreiduffy@gmail.com>
2018-02-24 11:07:00 +0300

    before sort

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 /***********************
  fancybox BEGIN
  ***********************/
-function init_fancy() {
-	$('.fancy').fancybox({
-		buttons: ['close'],
-		backFocus: false,
-		animationEffect: "zoom-in-out",
-		animationDuration: 300,
-		transitionEffect: "slide"
-	});
-}
-
 $(function () {
-	init_fancy();
+	$('.table-item').on('click',function () {
+		var modal = $(this).find('.modal');
+		$.fancybox.open({
+			src  : modal,
+			type : 'inline'
+		});
+	})
 });
 /***********************
  fancybox END
@@ -26,13 +22,24 @@ $(function () {
 	$('.scrollto').on('click', function () {
 		var elementClick = $(this).attr("href");
 		var destination = $(elementClick).offset().top;
-		$('html,body').stop().animate({scrollTop: destination}, 1000);
+		$('html,body').stop().animate({scrollTop: destination}, 600);
 		return false;
 	});
 });
 /***********************
  Прокрутка к секциям END
  ***********************/
+
+
+/***********************
+Niceselect BEGIN
+***********************/
+$(function () {
+	$('.style-select').niceSelect();
+});
+/***********************
+Niceselect END
+***********************/
 
 
 /***********************
@@ -144,4 +151,50 @@ $(function($){
 });
 /***********************
 table END
+***********************/
+
+
+
+/***********************
+Sorting BEGIN
+***********************/
+function sortTable(table,column,type) {
+	var tbody = grid.getElementsByTagName('tbody')[0];
+
+	// Составить массив из TR
+	var rowsArray = [].slice.call(tbody.rows);
+
+	// определить функцию сравнения, в зависимости от типа
+	var compare;
+
+	switch (type) {
+		case 'number':
+			compare = function(rowA, rowB) {
+				return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+			};
+			break;
+		case 'string':
+			compare = function(rowA, rowB) {
+				return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML;
+			};
+			break;
+	}
+
+	// сортировать
+	rowsArray.sort(compare);
+
+	// Убрать tbody из большого DOM документа для лучшей производительности
+	grid.removeChild(tbody);
+
+	// добавить результат в нужном порядке в TBODY
+	// они автоматически будут убраны со старых мест и вставлены в правильном порядке
+	for (var i = 0; i < rowsArray.length; i++) {
+		tbody.appendChild(rowsArray[i]);
+	}
+
+	grid.appendChild(tbody);
+
+}
+/***********************
+Sorting END
 ***********************/
